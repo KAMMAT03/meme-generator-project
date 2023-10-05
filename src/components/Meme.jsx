959f621b54f9f45be71641458bd8e0abdb1f9f0f
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2023-10-05 12:07:44 +0200

    small changes

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-
 export default function Meme(){
     return (
-        <div className="meme">
+        <form className="meme">
             <div className="meme-inputs">
                 <input className="meme-input" type="text" placeholder="Top text"/>
                 <input className="meme-input" type="text" placeholder="Bottom text"/>
             </div>
-            <button className="meme-button">Get a new meme image</button>
-        </div>
+            <button className="meme-button">Get a new meme image 🖼</button>
+        </form>
     )
 }
