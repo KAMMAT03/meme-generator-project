2d6380db8afd19252dde24afdb179d700fecbbb7
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2023-10-05 13:31:12 +0200

    state updates implemented

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,24 @@
+import React from "react";
+import memesData from "../memesData";
+
 export default function Meme(){
+    const [imgUrl, setImgUrl] = React.useState();
+
+    let getMemeImg = () => {
+        const length = memesData.data.memes.length;
+        const randomNum = Math.floor(Math.random() * length);
+        
+        setImgUrl(() => [memesData.data.memes[randomNum].url]);
+    }
+
     return (
-        <form className="meme">
+        <div className="meme">
             <div className="meme-inputs">
                 <input className="meme-input" type="text" placeholder="Top text"/>
                 <input className="meme-input" type="text" placeholder="Bottom text"/>
             </div>
-            <button className="meme-button">Get a new meme image 🖼</button>
-        </form>
+            <button onClick={getMemeImg} className="meme-button">Get a new meme image 🖼</button>
+            <img src={imgUrl} alt="" />
+        </div>
     )
 }
